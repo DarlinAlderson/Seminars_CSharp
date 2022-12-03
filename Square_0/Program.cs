00847faba913d0c0070288daa216e4fb82f73565
darlin_alderson <dash.cobain@gmail.com>
2022-12-03 20:16:32 +0300

    Решили первую задачу

---
+++ Square_0/Program.cs
@@ -0,0 +1,9 @@
+﻿// 0.Вывести квадрат числа
+
+Console.WriteLine("start");
+
+int number = 11;
+// int number = new Random().Next(1, 20); генератор случайных числел
+int result = number * number; //вычисление квадрата числа
+Console.WriteLine(result);
+Console.WriteLine("end");
